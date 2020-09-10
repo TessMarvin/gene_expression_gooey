481e3bb5da6783f88c8ada1e2cd2043e49c68148
TessMarvin <tmarvin@nd.edu>
2020-09-10 13:28:48 -0400

    Added on QC/TPM/parental boxgraphs

--- gui_dash.py
+++ gui_dash.py
@@ -8,6 +8,7 @@ import matplotlib.pyplot as plt
 import os
 import sys
 from argparse import ArgumentParser
+import numpy as np
 from gooey import Gooey
 from gooey import GooeyParser
 def gene_fun(gene, csv_files):
@@ -37,9 +38,151 @@ def gene_fun(gene, csv_files):
             name.set(xlabel = 'Time (hrs)', ylabel= 'Log2 (Fold Change)')
             num += 1
     plt.show()
+
+def sample_and_geneqc(expdata):
+    '''
+    Here, we'll remove poor samples and poor genes, and QC low read #s
+    Low reads are any gene/sample reading < 5 counts; those are zeroed out
+    Poor samples are defined as those with < 3000 genes with reads
+    Poor genes are those that appear in < 20% of samples
+    '''
+
+    # Curate Samples
+    #If there are less than 5 reads -- zero it out
+    expdata = expdata.mask(expdata < 5, 0)
+    #so np.count_nonzero with axis = 1 counts the number of nonzero counts for a gene across all columns (samples)
+    #basically, this counts how many samples this gene is in
+    genecounts = pd.Series(data = np.count_nonzero(expdata, axis = 1),
+                             index = expdata.index)
+    #so np.count_nonzero with axis = 0 counts the number of nonzero counts for a sample across all rows (genes)
+    #basically this counts how many genes with reads each sample has
+    samplecounts = pd.Series(np.count_nonzero(expdata,axis = 0),
+                          index = expdata.columns)
+    #this ensures that only the genes that are in more than 20% of samples are included
+    goodgenes = genecounts[genecounts/samplecounts.size > 0.2]
+
+    goodsamples = samplecounts[samplecounts > 3000]
+    #the .loc function creates a dataframe that only contains the goodgenes and goodsamples
+    allcur = expdata.loc[goodgenes.index, goodsamples.index]
+
+    return allcur
+
+def tpm_norm(expdata, probefile, gctdata, fraglength = 250):
+    '''
+    TPM is (reads / effective gene length (in kb)) / (sample reads / 1e6)
+    The elaborate nonsense below just calculates this, it's a bit of a
+    mess but hey that's programming
+    '''
+
+    # Read in probe metadata, subtract frag length,
+    # and convert to dict
+
+
+    probeinfo = pd.read_csv(probefile, usecols = [1,6])
+    probeinfo.index = probeinfo['Geneid']
+    probeinfo.drop(columns = 'Geneid', inplace = True)
+    probeinfo = probeinfo[probeinfo.index.isin(gctdata.index)]
+    probeinfo['Length'] = probeinfo['Length'].apply(lambda x: x-fraglength)
+
+    # Build output frame from input frame (cheating), calc length and
+    # build lookup from samplename to total counts
+
+    tpm = expdata.copy()
+    curated_tpm = expdata.copy()
+
+    # Iterate over rows- if non sample rows, just copy data, if sample
+    # rows, calculate TPM
+
+    # Note: it really doesn't like the way I did this and will throw
+    # warnings- it does work, it's fine, don't worry about it, if you
+    # are smarter than I am go ahead and rewrite it
+
+    for (col, data) in expdata.iteritems():
+
+        numreads = data.sum()
+
+        tempframe = pd.concat([probeinfo, data], axis = 1)
+
+        tempframe['rpkb'] = tempframe.iloc[:,1].divide(tempframe['Length'].divide(1000))
+        tempframe['tpm'] = (tempframe['rpkb'] / data.sum()) * 1e6
+        tempframe['tpm'] = tempframe['tpm'].clip(lower = 0)
+        curated_tpm.loc[:,col] = tempframe['tpm']
+    # Removing genes whose expression is ~0 in > 80% of samples
+    goodgenes=[]
+    for index, row in curated_tpm.iterrows():
+
+        if (np.count_nonzero(row > 0.1) / len(row) > 0.2):
+            goodgenes.append(index)
+
+    curated_tpm = curated_tpm[curated_tpm.index.isin(goodgenes)]
+    return curated_tpm
+
+def fix_names(countdata):
+    countdata = countdata.T
+    '''
+    This fixes the name specific issues in the NF54GFPxNHP4026 cross.
+    The names have changed several times and been recorded in different
+    formats, so I'll fix them with this.
+    '''
+    countdata.index = countdata.index.str.replace('\/','', regex = True)
+    countdata.index = countdata.index.str.replace('ND5A5', 'AC075', regex = True)
+    countdata.index = countdata.index.str.replace('ND6G8', 'AC125', regex = True)
+    countdata.index = countdata.index.str.replace('N1', '', regex = True)
+    countdata.index = countdata.index.str.replace('\\.', '', regex = True)
+    countdata.index = countdata.index.str.replace('_4026', '_NHP4026', regex = True)
+    countdata.index = countdata.index.str.replace('^4026', 'NHP4026', regex = True)
+    countdata.index = countdata.index.str.replace('2H9', 'AC030', regex = True)
+    countdata.index = countdata.index.str.replace('6E5', 'AC033', regex = True)
+     # This is a hot mess of regular expression that just converts the entire
+    # sample name to strain_##, where ## is the sampling timepoint. The formatting
+    # is very inconsistent throughout, so a mess of replacements need to be carefully
+    # made. This mess of substitutions makes them.
+    countdata.index = countdata.index.str.replace('^GF_PL[\d]+[a,b,c]{0,1}_', '',regex = True)
+    countdata.index = countdata.index.str.replace('[A,B,C]_', '', regex = True)
+    countdata.index = countdata.index.str.replace('_[d]+$', '', regex = True)
+    countdata.index = countdata.index.str.replace('_S.*', '', regex = True)
+    countdata.index = countdata.index.str.replace('_[0-9]{3,4}$', '', regex = True)
+    countdata.index = countdata.index.str.replace('hpi', '', regex = True)
+    countdata.index = countdata.index.str.replace('_T', '_', regex = True)
+    return countdata.T
+def parentalboxes(gene, df_correctnames):
+    '''
+    So this function makes box plots for the parental parasites at the three time points (4, 30, 44 hrs)
+    the parents are: NF54gfp x NHP4026
+    The data can be accessed like this normalized_fixed_names.loc['gene', 'rep']
+    e.g. normalized_fixed_names.loc['PF3D7_0100100', 'NF54gfp_44']
+    '''
+    timepoints = ['4', '30', '44']
+    NF54data= []
+    NHP4026data = []
+    for time in timepoints:
+        sample_name_NF = str('NF54gfp_' + time)
+        l = list(df_correctnames.loc[gene, sample_name_NF])
+        NF54data.append(l)
+        sample_name_NH = str('NHP4026_' + time)
+        m = list(df_correctnames.loc[gene, sample_name_NH])
+        NHP4026data.append(m)
+    data = {}
+    data['4 hpi'] = {}
+    data['30 hpi'] = {}
+    data['44 hpi'] = {}
+    num = 0
+    for k,v in data.items():
+        v['NF54gfp']= NF54data[num]
+        v['NHP4026']= NHP4026data[num]
+        num = num + 1
+    fig, axes = plt.subplots(ncols=3, sharey=True)
+    fig.subplots_adjust(wspace=0)
+    for ax, name in zip(axes, ['4 hpi', '30 hpi', '44 hpi']):
+        ax.boxplot([data[name][item] for item in ['NF54gfp', 'NHP4026']])
+        ax.set(xticklabels=['NF54gfp', 'NHP4026'], xlabel=name)
+        if name == '4 hpi':
+            ax.set(ylabel='Gene Expression (TPM)')
+        if name == '30 hpi':
+            ax.set(title=gene)
+        ax.margins(0.05)
+    plt.show()
 #the first argument is the gene name
-#next, the program searches recurisively thru the current directory for .csv files
-#or the files can be passed in by stdin as part of a pipeline
 @Gooey(
     program_name='Plasmodium falciparum Genomic Analysis',
     menu=[{
@@ -63,12 +206,19 @@ def main():
     parser = GooeyParser(description="Dashboard for Gene Expression Analysis of Plasmodium falciparum")
     #Here we allow for the entry of the gene of interest's name
     parser.add_argument('genename', help='Enter the gene you wish to analyze.')
-    #Here we allow for the selection of the data files to analyze
-    parser.add_argument("file_chooser", nargs='*', help = 'Choose Files to include in analysis.', widget='MultiFileChooser')
     #Next, we have a section where the user can select how they would like to analyze the data
     data_processing = parser.add_argument_group("Data Processing Options", "Customize Your Analysis")
-    #At the moment, there is only one way to analyze the data: a scatterplot of the gene expression over time
+    #First way to analyze the data: a scatterplot of the gene expression over time
     data_processing.add_argument('-scat', help='Enable Scatter Plot', action='store_true', widget='BlockCheckbox')
+    #Here we allow for the selection of the data files to analyze
+    data_processing.add_argument("-file_chooser", nargs='*', help = 'Choose Files to include in scatterplot analysis.', widget='MultiFileChooser')
+    #Second way to analyze the data: parental box subplots
+    data_processing.add_argument('-box', help='Enable Box Plots for Parental Replicates', action='store_true', widget='BlockCheckbox')
+    #Now we need a file chooser for the probe meta data
+    data_processing.add_argument("-probe_chooser", help = 'Choose Probe MetadataFile to include in boxplot analysis.', widget='FileChooser')
+    #We also need a file chooser to pick out the raw count data
+    data_processing.add_argument("-count_data", help = 'Choose Raw Count Data to include in boxplot analysis.', widget='FileChooser')
+
     #Now we parse all these arguments
     args = parser.parse_args()
     #Save the entry of the gene name
@@ -77,14 +227,31 @@ def main():
     scatter= args.scat
     #Save the file pathways that the user has selected
     csvfiles= args.file_chooser
-    #if no CSV files are found, fail gracefully and request the CSV files be in working directory
-    if(len(csvfiles) == 0):
-        print("Please ensure that you select files to analyze")
-        return(None)
-    else:
-        #If the user would like a scatter plot, produce one
-        if(scatter):
-            gene_fun(gene, csvfiles)
+    #Save whether or not the user would like to produce a parental box plot
+    boxer = args.box
+    #Save the probe MetadataFile
+    probefile = args.probe_chooser
+    #Save the raw count data
+    raw_counts_data = args.count_data
+    #make raw_couts_data a dataframe
+    gctdata = pd.read_csv(raw_counts_data, sep = '\t', index_col = 0)
 
+    #If the user would like a scatter plot, produce one
+    if(scatter):
+        #if no CSV files are found and we want to scatter, fail gracefully and request the CSV files be provided
+        if(len(csvfiles) == 0):
+            print("Please ensure that you select files to analyze")
+            return(None)
+        else:
+            gene_fun(gene, csvfiles)
+    if(boxer):
+        #Quality control the reads
+        qc_counts_data= sample_and_geneqc(gctdata)
+        #Normalize the QC data
+        normalized_counts= tpm_norm(qc_counts_data, probefile, gctdata)
+        #Fix the naming conventions
+        normalized_fixed_names = fix_names(normalized_counts)
+        #plot the box
+        parentalboxes(gene, normalized_fixed_names)
 if __name__ == '__main__':
    main()
